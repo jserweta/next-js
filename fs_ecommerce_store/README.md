4d64944904a42752c1ee4760ed610ae868e0262a
jserweta <jakub.serweta@gmail.com>
2024-09-20 14:58:32 +0200

    initial commit

---
+++ fs_ecommerce_store/README.md
@@ -0,0 +1,31 @@
+# Full Stack Ecommerce Store
+
+This project is full stack ecommerce store with admin dashboard.
+
+It is utilizing Next.js, Prisma for database and Tailwind for styling.
+
+### Install Dependencies
+
+```bash
+npm install
+```
+
+### Run the development server
+
+It will run on http://localhost:3000
+
+```bash
+npm run dev
+```
+
+### Build for Production
+
+```bash
+npm run build
+```
+
+### Preview Production Build
+
+```bash
+npm run start
+```
---
+++ fs_ecommerce_store/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Ecommerce Store",
+  description: "Full Stack Ecommerce Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn("min-h-screen font-sans antialiased", inter.variable)}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
